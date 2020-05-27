b84dfcc1f72497da0adafc65bfab52a5ab7b55cf
Avijit Nanda <avijitnanda14@gmail.com>
2020-05-28 00:35:50 +0530

    Updates

---
+++ precache-manifest.85b9b6147a296a479c548ada73afc104.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cedd622d5bc91f4dd91f28b1c56f6121",
+    "url": "/BookRead-Tracker/index.html"
+  },
+  {
+    "revision": "821d8baef54572208d65",
+    "url": "/BookRead-Tracker/static/css/main.95ef0747.chunk.css"
+  },
+  {
+    "revision": "488af9c12774a93af879",
+    "url": "/BookRead-Tracker/static/js/2.628b3b0b.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/BookRead-Tracker/static/js/2.628b3b0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "821d8baef54572208d65",
+    "url": "/BookRead-Tracker/static/js/main.1ec51fe0.chunk.js"
+  },
+  {
+    "revision": "a5c0d59f46193d33b91a",
+    "url": "/BookRead-Tracker/static/js/runtime-main.0109503d.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/BookRead-Tracker/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/BookRead-Tracker/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/BookRead-Tracker/static/media/arrow-drop-down.9d4e3f36.svg"
+  }
+]);
